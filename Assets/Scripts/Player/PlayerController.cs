198de712a5d817a003c4762455f1dc4a028d3169
Ben Chan <ben@chanhq.com>
2019-10-30 14:29:47 +0000

    Implemented player input

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,16 +7,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Vector3 _startPos;
 
+    [SerializeField]
+    private float downForce;
+    private bool isDead = false;
+    private Rigidbody2D rb;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+
         transform.position = _startPos;
     }
 
     
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            PushDown();
+        }
     }
 
     // Actions
@@ -24,11 +33,15 @@ public class PlayerController : MonoBehaviour
     void PushDown()
 	{
         // When input tap push object down
-	}
+        //...zero out the birds current y velocity before...
+        rb.velocity = Vector2.zero;
+        rb.AddForce(new Vector2(0, -downForce));
+    }
 
     void Die()
 	{
         // When collide with obstacle kill player
+        isDead = true;
 	}
 
 }
