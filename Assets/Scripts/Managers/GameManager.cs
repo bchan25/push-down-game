0c1ee894fc3ed00c491f93b9f9c63c70f746bf1f
Ben Chan <ben@chanhq.com>
2019-10-30 16:22:41 +0000

    Game Manager initialised

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int targetFrameRate = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial setup
+        Application.targetFrameRate = targetFrameRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
