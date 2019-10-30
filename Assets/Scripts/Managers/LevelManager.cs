acc5383235c2fc865c9956c14a5b0c10225d5d37
Ben Chan <ben@chanhq.com>
2019-10-30 15:27:28 +0000

    Level loaded from level manager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> levels;
+
+    private int levelIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadLevel(levelIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadLevel(int level)
+    {
+        Instantiate(levels[level], levels[level].transform.position, levels[level].transform.rotation);
+    }
+}
