0d16c862fba7b5d342d70626c8bfc10af0b0a222
Ben Chan <ben@chanhq.com>
2019-10-30 15:10:37 +0000

    Level move implemented

---
+++ Assets/Scripts/Level/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2f;
+
+
+    void Update()
+    {
+
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+    }
+}
