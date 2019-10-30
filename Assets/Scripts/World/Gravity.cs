9105068c42b8dce1afb7c870d8d2245a7cbd4a02
Ben Chan <ben@chanhq.com>
2019-10-30 13:38:53 +0000

    Change the gravity of the world

---
+++ Assets/Scripts/World/Gravity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script change the Gravity of the world
+public class Gravity : MonoBehaviour
+{
+    [SerializeField]
+    float gravity = 9.81f;
+
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0f, gravity);
+    }
+
+}
