b7140c47aed64d12f9763969a2d56d5195782e41
Ben Chan <ben@chanhq.com>
2019-10-30 14:22:49 +0000

    Set up start position

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _startPos;
+
+
+    void Start()
+    {
+        transform.position = _startPos;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    // Actions
+
+    void PushDown()
+	{
+        // When input tap push object down
+	}
+
+    void Die()
+	{
+        // When collide with obstacle kill player
+	}
+
+}
