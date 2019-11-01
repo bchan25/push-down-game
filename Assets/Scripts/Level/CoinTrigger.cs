381d50039da97643ba8a4308b0b7c5148d367519
Ben Chan <ben@chanhq.com>
2019-11-01 15:12:22 +0000

    Coin System Implemented

---
+++ Assets/Scripts/Level/CoinTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTrigger : MonoBehaviour
+{
+
+    // Reference to CoinManager
+    private CoinManager coinManager;
+
+    // Coin value
+    [SerializeField]
+    private int coinValue = 1;
+
+    void Start()
+    {
+        coinManager = FindObjectOfType<CoinManager>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            coinManager.Add(coinValue);
+            coinManager.UpdateLabel();
+            Destroy(gameObject);
+        }
+    }
+
+}
