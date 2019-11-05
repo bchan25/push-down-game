3404ed07606c4fc32ae3636f5d7175dcb722bfc4
Ben Chan <ben@chanhq.com>
2019-11-05 14:44:23 +0000

    Move score gui from score manager to ui manager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private ScoreManager scoreManager;
+
+    [SerializeField]
+    private Text scoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreLabel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScoreLabel()
+    {
+        scoreLabel.text = scoreManager.GetCurrentScore();
+    }
+}
